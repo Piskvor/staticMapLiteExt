aa1a0bbc7b741a332a4f3d7ceb2030e37534de37
Jan Martinec <jan@martinec.name>
2014-01-26 01:11:16 +0100

    added version and author info

--- staticmap.php
+++ staticmap.php
@@ -19,6 +19,10 @@
  *
  * @author Gerhard Koch <gerhard.koch AT ymail.com>
  *
+ * staticMapLiteEx 0.03
+ * Copyright 2013 Jan Martinec
+ * @author Jan Martinec <staticMapLiteEx AT martinec.name>
+ *
  * USAGE: 
  *
  *  staticmap.php?center=40.714728,-73.998672&zoom=14&size=512x512&maptype=mapnik&markers=40.702147,-74.015794,blues|40.711614,-74.012318,greeng|40.718217,-73.998284,redc
