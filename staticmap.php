19423e9e697f3b136f7e7ba492b392c9098881be
Jan Piskvor Martinec <github@piskvor.org>
2014-01-27 13:04:45 +0100

    add config example

--- staticmap.php
+++ staticmap.php
@@ -5,16 +5,24 @@ require_once(__DIR__ . '/staticmapliteex.class.php');
 $map = new staticMapLiteEx(array(
                             'request' => $_GET,
                             'headers' => $_SERVER,
+
+                            /* add your own sources as you see fit */
                             'mapSources' => array(
 	                            'mapnik' => 'http://tile.openstreetmap.org/{Z}/{X}/{Y}.png',
 	                            'osmarenderer' => 'http://c.tah.openstreetmap.org/Tiles/tile/{Z}/{X}/{Y}.png',
-	                            'cycle' => 'http://c.andy.sandbox.cloudmade.com/tiles/cycle/{Z}/{X}/{Y}.png'
                             ),
+
                             'cache' => array(
 	                            'http' => true,
 	                            'tile' => true,
 	                            'map' => true
                             ),
-                            'ua' => 'staticMapLiteEx/0.03'
+
+                            /* user agent to send in map tile requests */
+                            'ua' => 'staticMapLiteEx/0.03',
+
+                            /* max and min zoom only needed for "autozoom to markers" */
+                            'minZoom' => 12,
+                            'maxZoom' => 18
                            ));
 echo $map->showMap();
