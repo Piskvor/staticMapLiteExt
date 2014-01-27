4ecc649e06adaffc71b4a7994e3b9e0df4674e89
Jan Piskvor Martinec <github@piskvor.org>
2014-01-27 18:20:26 +0100

    re-add the license which was dropped in file split

--- staticmapliteex.class.php
+++ staticmapliteex.class.php
@@ -1,5 +1,34 @@
 <?php
 
+/**
+ * staticMapLite 0.03
+ *
+ * Copyright 2009 Gerhard Koch
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Gerhard Koch <gerhard.koch AT ymail.com>
+ *
+ * staticMapLiteEx 0.04
+ * Copyright 2013 Jan Martinec
+ * @author Jan "Piskvor" Martinec <staticMapLiteEx AT martinec.name>
+ *
+ * USAGE:
+ *
+ *  staticmap.php?center=40.714728,-73.998672&zoom=14&size=512x512&maptype=mapnik&markers=40.702147,-74.015794,blues|40.711614,-74.012318,greeng|40.718217,-73.998284,redc
+ *
+ */
+
 class staticMapLiteEx {
 
 	protected $tileSize = 256;
